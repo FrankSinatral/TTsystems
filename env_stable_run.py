0e65a8cb36c3cc6ecd82d8316ed8132ec59389a4
franksinatral <18208110225@163.com>
2023-12-18 19:48:55 +0800

    add sparse reward and add compute_reward

--- env_stable_run.py
+++ env_stable_run.py
@@ -81,14 +81,25 @@ def main():
     her_kwargs = dict(n_sampled_goal=4, goal_selection_strategy='future')
     from stable_baselines3 import HerReplayBuffer
     from stable_baselines3 import SAC as SAC_stable
-    model = SAC_stable('MultiInputPolicy', env, verbose=1, 
-                tensorboard_log="runs_stable_rl_tt", 
-                buffer_size=int(1e6),
-                learning_rate=1e-3,
-                learning_starts=1000,
-                gamma=0.95, batch_size=1024, tau=0.05,
-                policy_kwargs=dict(net_arch=[512, 512, 512]),
-                seed=60)
+    # model = SAC_stable('MultiInputPolicy', env, verbose=1, 
+    #             tensorboard_log="runs_stable_rl_tt", 
+    #             buffer_size=int(1e6),
+    #             learning_rate=1e-3,
+    #             learning_starts=1000,
+    #             gamma=0.95, batch_size=1024, tau=0.05,
+    #             policy_kwargs=dict(net_arch=[512, 512, 512]),
+    #             seed=60)
+    
+    model = SAC_stable('MultiInputPolicy', env, replay_buffer_class=HerReplayBuffer,
+            replay_buffer_kwargs=her_kwargs, verbose=1, 
+            tensorboard_log="runs_stable_rl_tt", 
+            buffer_size=int(1e6),
+            learning_rate=1e-3,
+            learning_starts=1000,
+            gamma=0.95, batch_size=1024, tau=0.05,
+            policy_kwargs=dict(net_arch=[512, 512, 512]),
+            seed=20)
+    
     LEARNING_STEPS = 4e6 # @param {type: "number"}
     model.learn(int(LEARNING_STEPS), tb_log_name="sac")
     model.save("stable_rl_save/")
