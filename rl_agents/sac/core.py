10d89c88839724e426f2a0b0e33774dea0d771f4
hdys1100100001 <huangzhu20@mails.tsinghua.edu.cn>
2024-02-29 14:26:21 +0800

    ResNet 18 DQN Agent

--- rl_agents/sac/core.py
+++ rl_agents/sac/core.py
@@ -6,6 +6,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.normal import Normal
 
+# For ResNet agent
+import random
+from operator import add
+import collections
+import math
+
 
 def combined_shape(length, shape=None):
     if shape is None:
@@ -97,3 +103,216 @@ class MLPActorCritic(nn.Module):
             a, _ = self.pi(obs, deterministic, False)
             # I have to change here for GPU
             return a.cpu().numpy()
+
+# ResNet18 agent
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNetAgent(nn.Module):
+
+    def __init__(self, params):
+        super(ResNetAgent, self).__init__()
+
+        self.num_classes = 3
+        block = BasicBlock
+        layers = [2, 2, 2, 2]
+        self.graph_shape = (7, 22, 22)
+        self.vector_shape = 11
+
+        self.reward = 0
+        self.gamma = 0.9
+        self.short_memory = np.array([])
+        self.agent_target = 1
+        self.agent_predict = 0
+        self.learning_rate = params['learning_rate']
+        self.epsilon = 1
+        self.actual = []
+        self.zero_layer = params['graph_output_size']
+        self.first_layer = params['first_layer_size']
+        self.second_layer = params['second_layer_size']
+        self.third_layer = params['third_layer_size']
+        self.memory = collections.deque(maxlen=params['memory_size'])
+        self.weights = params['weights_path']
+        self.load_weights = params['load_weights']
+        self.optimizer = None
+
+        self.inplanes = 16
+        self.conv1 = nn.Conv2d(7, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.layer1 = self._make_layer(block, self.inplanes, layers[0])
+        self.layer2 = self._make_layer(block, 2 * self.inplanes, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 4 * self.inplanes, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 8 * self.inplanes, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(3, stride=1)
+        self.fc = nn.Linear(8 * self.inplanes * block.expansion, self.zero_layer)
+
+        self.f1 = nn.Linear(self.zero_layer + self.vector_shape, self.first_layer)
+        self.f2 = nn.Linear(self.first_layer, self.second_layer)
+        self.f3 = nn.Linear(self.second_layer, self.third_layer)
+        self.f4 = nn.Linear(self.third_layer, self.num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        
+        if self.load_weights:
+            self.model = self.load_state_dict(torch.load(self.weights))
+            print("weights loaded")
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x, y):
+        # for input size: 7 * 22 * 22
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc(x))
+
+        x = F.relu(self.f1(torch.cat((x, y), 1)))
+        x = F.relu(self.f2(x))
+        x = F.relu(self.f3(x))
+        x = F.relu(self.f4(x))
+
+        x = F.softmax(x, dim=-1)
+        return x
+    
+    def get_state(self):
+        """
+        Return the state graph and state vector.
+        """
+        pass
+
+    def set_reward(self):
+        """
+        Return the reward.
+        """
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        """
+        Store the <state, action, reward, next_state, is_done> tuple in a 
+        memory buffer for replay memory.
+        """
+        self.memory.append((state, action, reward, next_state, done))
+
+    def replay_new(self, memory, batch_size, short_batch):
+        """
+        Replay memory.
+        """
+        batch_size = min(len(memory), batch_size)
+        state_old_cont = [torch.zeros((batch_size, self.graph_shape[0], self.graph_shape[1], self.graph_shape[2]), dtype=torch.float32), 
+                          torch.zeros((batch_size, self.vector_shape), dtype=torch.float32)]
+        state_new_cont = [torch.zeros((batch_size, self.graph_shape[0], self.graph_shape[1], self.graph_shape[2]), dtype=torch.float32), 
+                          torch.zeros((batch_size, self.vector_shape), dtype=torch.float32)]
+        action_cont = torch.zeros((batch_size, self.num_classes), dtype=torch.float32)
+        reward_cont = torch.zeros(batch_size, dtype=torch.float32)
+        done_cont = torch.zeros(batch_size, dtype=torch.float32)
+        if len(memory) > batch_size:
+            minibatch = random.sample(memory, batch_size)
+        else:
+            minibatch = memory
+            
+        slot = 0
+        for state, action, reward, next_state, done in minibatch:
+            state_old_cont[0][slot] = torch.tensor(state[0], dtype=torch.float32)
+            state_old_cont[1][slot] = torch.tensor(state[1], dtype=torch.float32)
+            action_cont[slot] = torch.tensor(action, dtype=torch.float32)
+            state_new_cont[0][slot] = torch.tensor(next_state[0], dtype=torch.float32)
+            state_new_cont[1][slot] = torch.tensor(next_state[1], dtype=torch.float32)
+            reward_cont[slot] = reward
+            done_cont[slot] = 0 if done else 1
+            slot += 1
+            if slot == short_batch:
+                self.train()
+                torch.set_grad_enabled(True)
+                target = reward_cont + self.gamma * torch.max(self.forward(state_new_cont[0], state_new_cont[1]), 1)[0] * done
+                output = self.forward(state_old_cont[0], state_old_cont[1])
+                target_f = output.clone()
+                target_f[:, np.argmax(action_cont, 1)] = target
+                target_f.detach()
+                self.optimizer.zero_grad()
+                loss = F.mse_loss(output, target_f)
+                loss.backward()
+                self.optimizer.step()
+                slot = 0
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        """
+        Train the DQN agent on the <state, action, reward, next_state, is_done>
+        tuple at the current timestep.
+        """
+        self.train()
+        torch.set_grad_enabled(True)
+        target = reward + self.gamma * torch.max(self.forward(next_state[0], next_state[1]), 1)[0] * done
+        output = self.forward(state[0], state[1])
+        target_f = output.clone()
+        target_f[[range(action.shape[0]), np.argmax(action, 1)]] = target
+        target_f.detach()
+        self.optimizer.zero_grad()
+        loss = F.mse_loss(output, target_f)
+        loss.backward()
+        self.optimizer.step()
+        return loss
