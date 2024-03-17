59d554cb7025308a1cf1da11931a57705f7d3641
FrankSinatral <18208110225@163.com>
2024-03-17 23:15:12 +0800

    version confirmed again

--- rl_agents/sac/sacastar.py
+++ rl_agents/sac/sacastar.py
@@ -43,7 +43,7 @@ def find_expert_trajectory(o, vehicle_type):
         "plot_final_path": False,
         "plot_rs_path": False,
         "plot_expand_tree": False,
-        "mp_step": 12,
+        "mp_step": 12, # Important
         "N_steps": 20, # Important
         "range_steer_set": 20,
         "max_iter": 50,
