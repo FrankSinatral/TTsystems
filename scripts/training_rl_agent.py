5ce7a793c69b776efc7daf5d39b0e5a76fe6de01
FrankSinatral <18208110225@163.com>
2024-06-27 20:05:50 +0800

    new script for training rl agent

--- scripts/training_rl_agent.py
+++ scripts/training_rl_agent.py
@@ -1,28 +1,21 @@
 import sys
 import os
-import torch.nn as nn
-import gymnasium as gym
-import pprint
+
 import yaml
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+
 import rl_agents as agents
-import tractor_trailer_envs as tt_envs
-from tractor_trailer_envs import register_tt_envs
-from datetime import datetime
-from rl_agents.sac import core
-
-def gym_reaching_tt_env_fn(config: dict): 
-    return gym.make("tt-reaching-v0", config=config)
-
-def gym_meta_reaching_tt_env_fn(config: dict):
-    return gym.make("tt-meta-reaching-v0", config=config)
-
 # Apply our new-tt planning env to this agent for training
 def gym_tt_planning_env_fn(config: dict):
+    import gymnasium as gym
+    import tractor_trailer_envs as tt_envs
+    if not hasattr(gym_tt_planning_env_fn, "envs_registered"):
+        tt_envs.register_tt_envs()
+        gym_tt_planning_env_fn.envs_registered = True
     return gym.make("tt-planning-v0", config=config)
 
 def main():
-    with open("configs/agents/training/rl1_att.yaml", 'r') as file:
+    with open("configs/agents/training/rl1_obs_mlp.yaml", 'r') as file:
         config_algo = yaml.safe_load(file)
     agent = agents.SAC_ASTAR_META_NEW(env_fn=gym_tt_planning_env_fn,
         config=config_algo)
