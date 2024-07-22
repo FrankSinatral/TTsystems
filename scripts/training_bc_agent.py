498b49a086cc2adfefe1e2ac2683007210118a93
FrankSinatral <18208110225@163.com>
2024-07-22 16:31:23 +0800

    add bc agent

---
+++ scripts/training_bc_agent.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+import yaml
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+
+import rl_agents as agents
+# Apply our new-tt planning env to this agent for training
+def gym_tt_planning_env_fn(config: dict):
+    import gymnasium as gym
+    import tractor_trailer_envs as tt_envs
+    if not hasattr(gym_tt_planning_env_fn, "envs_registered"):
+        tt_envs.register_tt_envs()
+        gym_tt_planning_env_fn.envs_registered = True
+    return gym.make("tt-planning-v0", config=config)
+
+def main():
+    config_filename = "configs/agents/training/bc.yaml"
+    with open(config_filename, 'r') as file:
+        config_algo = yaml.safe_load(file)
+    agent = agents.BC(env_fn=gym_tt_planning_env_fn,
+        config=config_algo)
+    agent.run()
+        
+    print(1)
+
+if __name__ == "__main__":
+    main()
+    
